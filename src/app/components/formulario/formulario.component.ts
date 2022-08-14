3a78fa9c6c455c6ab55717341bf8e68b0ce8b3e9
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-08-14 15:01:56 +0200

    peticion fetch configurada

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import swal from'sweetalert2';
 
 @Component({
     selector:'formulario',
@@ -6,7 +7,9 @@ import {Component} from '@angular/core';
 })
 export class Formulario{
     public user:any;
-    public validacion:boolean
+    public datos:any;
+    public validacion:boolean;
+    
     
     constructor(){
         
@@ -18,85 +21,106 @@ export class Formulario{
             cvc:''
 
         }
+        this.datos={
+            name:'',
+            number:'',
+            fecha:'',
+            cvc:''
+        }
         this.validacion=false
+        
+
     }
     onSubmit(){
-        let name=""
-        let number=""
-        let fecha=""
-        let cvc=""
-        let noValidadoName=false
-        let noValidadoNumber=false
-        let noValidadoNumberLen=false
-        let noValidadoFechaNumber=false
-        let noValidadoCvc=false
-        let noValidadoCvcNumber=false
-        let noValidadoFecha=false
+        
+        let noValidado=false
         //We validate if name card is only letters
         let numbers="0123456789"
         for(let i of this.user.name){
             if(numbers.indexOf(i)!=-1){
                 console.log(i)
-                noValidadoName=true
+                noValidado=true
                 break;
             }else{
-                name=this.user.name
+                this.datos.name=this.user.name
             }
         }
         //We validate if number card is only number
         if(!isNaN(this.user.number)){
-            number=this.user.number
+            this.datos.number=this.user.number
         }else{
-            noValidadoNumber=true
+            noValidado=true
         }
         //We validate is number card length is 16 digits
         if(this.user.number.length==16){
-            number=this.user.number
+            this.datos.number=this.user.number
         }else{
-            noValidadoNumberLen=true
+            noValidado=true
         }
         //We validate is dates length are 2 digits
         if(this.user.fecha1.length==2 && this.user.fecha2.length==2){
-            fecha=this.user.fecha1+"/"+this.user.fecha2
+            this.datos.fecha=this.user.fecha1+"/"+this.user.fecha2
         }else{
-            noValidadoFecha=true
+            noValidado=true
         }
         //We validate if dates are only numbers
         if((!isNaN(this.user.fecha1))&&(!isNaN(this.user.fecha2))){
-            fecha=this.user.fecha1+"/"+this.user.fecha2
+            this.datos.fecha=this.user.fecha1+"/"+this.user.fecha2
         }else{
-            noValidadoFechaNumber=true
+            noValidado=true
         }
         //We validate if cvc length is 3 digits
         if(this.user.cvc.length==3){
-            cvc=this.user.cvc
+            this.datos.cvc=this.user.cvc
         }else{
-            noValidadoCvc=true
+            noValidado=true
         }
         //We validate if cvc is only number
         if(!isNaN(this.user.cvc)){
-            cvc=this.user.cvc
+            this.datos.cvc=this.user.cvc
         }else{
-            noValidadoCvcNumber=true
+            noValidado=true
         }
 
         //Results of validations
-        if(noValidadoNumberLen===true){
-            alert("Invalid format, number card is 16 digits")
-        }else if(noValidadoName===true){
-            alert("Invalid format, name is letters only")
-        }else if(noValidadoNumber===true){
-            alert("Invalid format, number card is only number format")
-        }else if(noValidadoFechaNumber===true){
-            alert("Invalid format, date is only number format")
-        }else if(noValidadoFecha===true){
-            alert("Invalid format, date is only 2 characters")
-        }else if(noValidadoCvc===true){
-            alert("Invalid format, date is only 3 characters")
-        }else if(noValidadoCvcNumber===true){
-            alert("Invalid format, cvc is only number format")
+        if(noValidado===true){
+        
+        swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href="">Your data is wrong</a>'
+        })
+            
         }else{
-            alert("We send your data")
+            
+            //Se conecta con la API Rest y se guardan los datos del formulario mediante post
+            fetch('http://localhost:3500/api/save',{
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json;charset=UTF-8',
+                },
+                body:JSON.stringify({
+                    name:this.datos.name,
+                    number:this.datos.number,
+                    date:this.datos.fecha,
+                    cvc:this.datos.cvc
+                })
+                
+        })
+            .then((res)=>res.json())
+            .then((data)=>{
+                console.log("Datos guardados: "+data)
+            })
+            .catch((error)=>{
+                console.log("Error en la petición "+error) 
+            })
+
+            swal.fire({
+                icon: 'success',
+                text: 'We send your data',
+                
+            })
             this.validacion=true
         }
         
